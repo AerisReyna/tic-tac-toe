7a8fde76d12a648f8d5d5ab205a71a7549996f4a
Aurunos <emberwolfdev@gmail.com>
2020-06-01 00:52:36 -0400

    Basic game functionality. Unlikely to continue from here.

---
+++ app/src/main/java/com/emberforge/tictactoe/MainActivity.kt
@@ -0,0 +1,145 @@
+package com.emberforge.tictactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttons: Array<Button>
+    private lateinit var resetButton: Button
+    private var isPlayerOneTurn = true
+    private lateinit var playerOneWins: TextView
+    private lateinit var playerTwoWins: TextView
+    private var gameOver = false
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttons = getButtons()
+        assignButtonCallbacks()
+
+        resetButton = findViewById(R.id.reset_button)
+        resetButtons()
+        resetButton.setOnClickListener{
+            resetButtons()
+        }
+
+        playerOneWins = findViewById(R.id.player_one_score)
+        playerTwoWins = findViewById(R.id.player_two_score)
+    }
+
+    private fun getButtons(): Array<Button> {
+            return arrayOf(
+                findViewById(R.id.button_1),
+                findViewById(R.id.button_2),
+                findViewById(R.id.button_3),
+                findViewById(R.id.button_4),
+                findViewById(R.id.button_5),
+                findViewById(R.id.button_6),
+                findViewById(R.id.button_7),
+                findViewById(R.id.button_8),
+                findViewById(R.id.button_9)
+        )
+    }
+
+    private fun assignButtonCallbacks() {
+        for (button in buttons) {
+            button.setOnClickListener {
+                if (!gameOver) {
+                    if (button.text == "X" || button.text == "O") {
+                        Toast.makeText(
+                            applicationContext,
+                            "Square is already occupied.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        if (isPlayerOneTurn) {
+                            button.text = "X"
+                        } else {
+                            button.text = "O"
+                        }
+                        checkForWin()
+                        isPlayerOneTurn = !isPlayerOneTurn
+                    }
+                } else {
+                    Toast.makeText(applicationContext, "Game Over. Hit 'Reset'!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun checkForWin() {
+        var playerWon = false
+        if (buttons[0].text != " " && buttons[0].text == buttons[1].text && buttons[1].text == buttons[2].text) {
+            playerWon = true
+        } else if (buttons[3].text != " " && buttons[3].text == buttons[4].text && buttons[4].text == buttons[5].text) {
+            playerWon = true
+        } else if (buttons[6].text != " " && buttons[6].text == buttons[7].text && buttons[7].text == buttons[8].text) {
+            playerWon = true
+        } else if (buttons[0].text != " " && buttons[0].text == buttons[3].text && buttons[3].text == buttons[6].text) {
+            playerWon = true
+        } else if (buttons[1].text != " " && buttons[1].text == buttons[4].text && buttons[4].text == buttons[7].text) {
+            playerWon = true
+        } else if (buttons[2].text != " " && buttons[2].text == buttons[5].text && buttons[5].text == buttons[8].text) {
+            playerWon = true
+        } else if (buttons[0].text != " " && buttons[0].text == buttons[4].text && buttons[4].text == buttons[8].text) {
+            playerWon = true
+        } else if (buttons[2].text != " " && buttons[2].text == buttons[4].text && buttons[4].text == buttons[6].text) {
+            playerWon = true
+        }
+
+        if (playerWon && isPlayerOneTurn) {
+            playerOneWins.text = (Integer.parseInt(playerOneWins.text.toString()) + 1).toString()
+            gameOver = true
+            Toast.makeText(applicationContext, "Player One wins! Hit 'reset' to play again.", Toast.LENGTH_SHORT).show()
+        } else if (playerWon && !isPlayerOneTurn) {
+            playerTwoWins.text = (Integer.parseInt(playerTwoWins.text.toString()) + 1).toString()
+            gameOver = true
+            Toast.makeText(applicationContext, "Player Two wins! Hit 'reset' to play again.", Toast.LENGTH_SHORT).show()
+        }
+
+        if (!playerWon) {
+            for (button in buttons) {
+                if (button.text == " ") {
+                    return
+                }
+            }
+            gameOver = true
+            Toast.makeText(applicationContext, "Tie! Hit 'reset' to play again.", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun resetButtons() {
+        for (button in buttons) {
+            button.text = " "
+        }
+        isPlayerOneTurn = true
+        gameOver = false
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putBoolean("isPlayerOneTurn", isPlayerOneTurn)
+        outState.putCharSequence("playerOneWins", playerOneWins.text)
+        outState.putCharSequence("playerTwoWins", playerTwoWins.text)
+        outState.putBoolean("gameOver", gameOver)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        isPlayerOneTurn = savedInstanceState.getBoolean("isPlayerOneTurn")
+        playerOneWins.text = savedInstanceState.getCharSequence("playerOneWins")
+        playerTwoWins.text = savedInstanceState.getCharSequence("playerTwoWins")
+        gameOver = savedInstanceState.getBoolean("gameOver")
+    }
+}
